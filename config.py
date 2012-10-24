dda1a8ec097edf35a153be64a618427f3735b45f
Svante Kvarnström <sjk@ankeborg.nu>
2012-10-24 23:48:53 +0200

    Changed error message wording to include reference "general block"

--- config.py
+++ config.py
@@ -86,7 +86,7 @@ if __name__ == "__main__":
     try:
         conf = ConfigGeneral()
     except ErrorUnknownConfigOption as e:
-        print "Unknown config options were found:"
+        print "Unknown config options were found in the general block:"
         print e.conf
         sys.exit()
 
